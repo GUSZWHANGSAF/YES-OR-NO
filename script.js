86b1b0c2bff6943b85f3ccc40dd01bd94f433995
GUSZWHANGSAF <bagoszraisyah@gmail.com>
2025-06-02 20:42:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -54,16 +54,14 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Beneran Nih?",
+    "Serius Bener??",
+    "Udah positif?",
+    "serius? ...",
+    "makasi!",
+    "kamu juga cantik kok..",
+    "maaf kalo aku banyak kurangnya...",
+    "I will be very very very happy...", 
 ];
 
 let messageIndex = 0;
